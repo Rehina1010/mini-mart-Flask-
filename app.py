7888b41f367c7e5a114a68be1a6f68163ea1896a
Regina0101 <ritodo1010@gmail.com>
2024-09-27 15:59:33 +0100

    changes

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ class ProductImageView(ModelView):
 admin.add_view(ProductImageView(ProductImage, db.session))
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=8080)
